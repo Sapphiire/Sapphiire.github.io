008ad5cb49de658772cb9a061414513e29c404eb
Sapphiire <zakharovvadzim@yandex.by>
2021-03-16 16:19:15 +0300

    add: animate

--- js/index.js
+++ js/index.js
@@ -1,49 +1,58 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burger_menu = document.querySelector('.menu').querySelector('span')
-    const modal_nav = document.querySelector('.modal-nav')
-    const nav__items = document.querySelectorAll('.nav__item')
-    
-    burger_menu.addEventListener('click', (e) => {
-        modal_nav.classList.toggle('active')
-        if(burger_menu.innerHTML == "close")
-            burger_menu.innerHTML = "menu";
-        else 
-            burger_menu.innerHTML = "close";
-    })
-    Array.from(nav__items).forEach(el => {
-        el.addEventListener('click', e => {
-            modal_nav.classList.remove('active')
-            burger_menu.innerHTML = "menu"
-        })
-        
-    })
-    document.addEventListener('keydown', (e) => {
-        if(e.keyCode == 27) {
-            modal_nav.classList.remove('active')
-            burger_menu.innerHTML = "menu"
-        }
-    })
+//variables
 
+const burger = document.querySelector(".burger");
+const modal_nav = document.querySelector(".modal-nav");
+const modal_links = modal_nav.querySelectorAll("a");
+const animates_items = document.querySelectorAll(".animate")
 
-    const animated_scroll_items = document.querySelectorAll('.animated-item');
+// events
 
-    const scrollAnimation = () => {
-        let scrollTriger = (3*window.innerHeight / 4) + window.scrollY;
-        animated_scroll_items.forEach(el => {
-            let scrollOffSet = el.offsetParent.offsetTop + el.offsetHeight/3;
+burger.addEventListener('click', (e) => {
+    toggle(burger)
+    toggle(modal_nav);
+}) 
 
-            if(scrollTriger >= scrollOffSet) {
-                el.classList.add('animated');
-            }
-            
-        })
-    }
-
-    scrollAnimation();
-    window.addEventListener('scroll', () => {
-        scrollAnimation();
+modal_links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        toggle(burger);
+        toggle(modal_nav);
     })
 })
 
+document.addEventListener('keydown', (e) => {
+    if(e.keyCode == 27) {
+        modal_nav.classList.remove('active');
+        burger.classList.remove('active');
+    }
+})
 
 
+window.addEventListener('scroll', (e) => {
+    scrollAnimation();
+})
+
+// functions
+
+const toggle = (item) => {
+    if(item.classList.contains("active"))
+        item.classList.remove("active");
+    else
+        item.classList.add("active");
+}
+
+
+const scrollAnimation = () => {
+    let scrollTriger = (1*window.innerHeight / 2) + window.scrollY;
+    animates_items.forEach(el => {
+        let scrollOffSet = el.offsetParent.offsetTop + el.offsetHeight/4;
+
+        if(scrollTriger >= scrollOffSet) {
+            el.classList.add('animated');
+        }
+        
+    })
+}
+
+// start 
+
+scrollAnimation();
